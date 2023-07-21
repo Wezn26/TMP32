c9f7e480c04ffb643588dd58e6677b4fe1df7f1c
Leonid Second Comp <li_che_in@mail.ru>
2023-07-21 11:56:12 +0300

    Create App/Creational/Singleton.php

---
+++ App/Creational/Singleton.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Creational;
+
+/**
+ * Description of Singleton
+ *
+ * @author leonid
+ */
+class Singleton 
+{
+    //put your code here
+}
